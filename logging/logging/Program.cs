46997ad822345f32eef9d6bea882f9dd8cb416cc
kalluris2 <kallurisandeep437@gmail.com>
2018-01-07 18:07:45 -0600

    logging

---
+++ logging/logging/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[assembly:log4net.Config.XmlConfigurator(Watch=true)]
+
+namespace logging
+{
+    class Program
+    {
+        public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            log.Error("This is my error message");
+            Console.ReadLine();
+        }
+    }
+}
