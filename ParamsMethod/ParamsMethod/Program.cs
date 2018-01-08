75438d4398bf1ba6c4120f70efe36f6d8ee738fc
kalluris2 <kallurisandeep437@gmail.com>
2018-01-08 14:07:55 -0600

    params keyword

---
+++ ParamsMethod/ParamsMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ParamMethod(1, 2, 3);
+            ParamMethod();
+            Console.ReadLine();
+        }
+        public static void ParamMethod(params int[] Digits)
+        {
+            Console.WriteLine("the no of elements in Digits is {0}", Digits.Length);
+            foreach(int i in Digits)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
